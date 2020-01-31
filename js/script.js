05f8a05002df0647e80621e5dd900606ff96a7c2
James P <james.piaggio13@gmail.com>
2020-01-31 11:30:15 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ const pageDiv = document.querySelector('.page');
 const searchResults = [];
 const itemsPerPage = 10;
 
+// Pagination div
+const div = document.createElement('div');
+const ul = document.createElement('ul');
+div.appendChild(ul);
+div.className = 'pagination';
+pageDiv.appendChild(div);
+
 // Appends searchbar to html
 const searchDiv = document.createElement('div');
 const searchField = document.createElement('input');
@@ -53,16 +60,18 @@ const showPage = (list, page) => {
             list[i].style.display = 'none';
         }
     }  
+    div.addEventListener('click', (e) => {
+        const pageLink = event.target;
+        const activeLink = document.querySelector('.active');
+        activeLink.className = '';
+        pageLink.className = 'active';
+        showPage(list, pageLink.textContent);    
+    });
 }
 
 // appendPageLinks function
 const appendPageLinks = (list) => {
     const pageAmount = Math.ceil(list.length / itemsPerPage);
-    const div = document.createElement('div');
-    div.className = 'pagination';
-    pageDiv.appendChild(div);
-    const ul = document.createElement('ul');
-    div.appendChild(ul);
     for (let i = 0; i < pageAmount; i++) {
         const li = document.createElement('li');
         const a = document.createElement('a');
@@ -72,14 +81,7 @@ const appendPageLinks = (list) => {
         ul.appendChild(li);
     }
     ul.firstElementChild.firstElementChild.className = 'active';   
-    showPage(studentItem, 1);
-    div.addEventListener('click', (e) => {
-        const pageLink = event.target;
-        const activeLink = document.querySelector('.active');
-        activeLink.className = '';
-        pageLink.className = 'active';
-        showPage(studentItem, pageLink.textContent);    
-    });
+    showPage(list, 1);
 }
 
 // Event Listeners
