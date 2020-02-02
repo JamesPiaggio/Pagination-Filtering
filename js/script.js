bf261066b2cba8a8e489e86496c5c2e73d3b14b4
James P <james.piaggio13@gmail.com>
2020-02-01 21:28:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,15 +5,10 @@ FSJS project 2 - List Filter and Pagination
 // Global variables
 const studentItem = document.querySelectorAll('.student-item');
 const pageDiv = document.querySelector('.page');
-const searchResults = [];
+let searchResults = [];
 const itemsPerPage = 10;
 
-// Pagination div
-const div = document.createElement('div');
-const ul = document.createElement('ul');
-div.appendChild(ul);
-div.className = 'pagination';
-pageDiv.appendChild(div);
+
 
 // Appends searchbar to html
 const searchDiv = document.createElement('div');
@@ -31,8 +26,8 @@ searchField.placeholder = 'Search for students...';
 // resetPageLinks function
 const resetPageLinks = () => {
     // Removes current Pagination links
+    document.getElementsByClassName("pagination")[0].innerHTML = ' '; 
     pageDiv.removeChild(document.querySelector('.pagination'));
-    studentItem.style.display = 'none';
 }
 
 // searchList function
@@ -46,7 +41,13 @@ const searchList = (searchField, list) => {
             student.style.display = 'none';
             }
     }
-    return searchResults;
+    appendPageLinks(searchResults);
+    if (searchField.value === '') {
+        searchResults = [];
+        resetPageLinks();
+        appendPageLinks(studentItem);
+    }
+    
 }
 
 // showPage function
@@ -60,7 +61,7 @@ const showPage = (list, page) => {
             list[i].style.display = 'none';
         }
     }  
-    div.addEventListener('click', (e) => {
+    document.querySelector('.pagination').addEventListener('click', (e) => {
         const pageLink = event.target;
         const activeLink = document.querySelector('.active');
         activeLink.className = '';
@@ -72,32 +73,36 @@ const showPage = (list, page) => {
 // appendPageLinks function
 const appendPageLinks = (list) => {
     const pageAmount = Math.ceil(list.length / itemsPerPage);
+    // Pagination div
+    const div = document.createElement('div');
+    const ul = document.createElement('ul');
+    div.className = 'pagination';
+    pageDiv.appendChild(div);
     for (let i = 0; i < pageAmount; i++) {
         const li = document.createElement('li');
         const a = document.createElement('a');
+        div.appendChild(ul);
         a.href = '#';
         a.textContent = i + 1;
         li.appendChild(a);
         ul.appendChild(li);
     }
+    div.appendChild(ul);
+    pageDiv.appendChild(document.querySelector('.pagination'));
     ul.firstElementChild.firstElementChild.className = 'active';   
     showPage(list, 1);
 }
 
 // Event Listeners
 searchDiv.addEventListener('click', (e) => {
-    searchList(searchField, studentItem);
     resetPageLinks();
-    appendPageLinks(searchResults);
-    showPage(searchResults, 1);
+    searchList(searchField, studentItem);
     });
 
 
 searchDiv.addEventListener('keyup', () => {
-    searchList(searchField, studentItem);
     resetPageLinks();
-    appendPageLinks(searchResults);
-    showPage(searchResults, 1);
+    searchList(searchField, studentItem);
 });
 
 appendPageLinks(studentItem);
